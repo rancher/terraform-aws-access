618f61c80efb330d4931740a854adf39731e8be5
matttrach <matttrach@gmail.com>
2023-08-25 11:42:28 -0500

    fix nix flake, add security_group_name which matches the tag rather than the attribute
    Signed-off-by: matttrach <matttrach@gmail.com>

--- modules/security_group/outputs.tf
+++ modules/security_group/outputs.tf
@@ -1,6 +1,9 @@
 output "id" {
   value = (local.select == 1 ? data.aws_security_group.selected[0].id : aws_security_group.new[0].id)
 }
+output "name" {
+  value = (local.select == 1 ? data.aws_security_group.selected[0].tags.Name : aws_security_group.new[0].tags.Name)
+}
 output "security_group" {
   value = (local.select == 1 ? data.aws_security_group.selected[0] : aws_security_group.new[0])
 }
