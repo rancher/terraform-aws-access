68be7e1120c87f35f6572c66d1d4565f78a2827e
matttrach <matttrach@gmail.com>
2023-09-26 17:05:24 -0500

    add the option to set the availability zone when generating a subnet and test
    Signed-off-by: matttrach <matttrach@gmail.com>

--- modules/subnet/main.tf
+++ modules/subnet/main.tf
@@ -1,10 +1,11 @@
 locals {
-  select = (var.cidr == "" ? 1 : 0)
-  create = (var.cidr != "" ? 1 : 0)
-  name   = var.name
-  cidr   = var.cidr
-  vpc_id = var.vpc_id
-  owner  = var.owner
+  select            = (var.cidr == "" ? 1 : 0)
+  create            = (var.cidr != "" ? 1 : 0)
+  name              = var.name
+  cidr              = var.cidr
+  vpc_id            = var.vpc_id
+  owner             = var.owner
+  availability_zone = var.availability_zone
 }
 
 data "aws_subnet" "selected" {
@@ -15,9 +16,10 @@ data "aws_subnet" "selected" {
   }
 }
 resource "aws_subnet" "new" {
-  count      = local.create
-  vpc_id     = local.vpc_id
-  cidr_block = local.cidr
+  count             = local.create
+  vpc_id            = local.vpc_id
+  cidr_block        = local.cidr
+  availability_zone = local.availability_zone
   tags = {
     Name  = local.name
     Owner = local.owner
