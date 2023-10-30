45c7b3351aed0c8f34e641c0f93d4c9133f0b4c9
matttrach <matttrach@gmail.com>
2023-10-30 15:18:12 -0500

    feat: generate new security group rules and add automatic updates
    Signed-off-by: matttrach <matttrach@gmail.com>

--- modules/security_group/main.tf
+++ modules/security_group/main.tf
@@ -27,89 +27,70 @@ resource "aws_security_group" "new" {
     Name  = local.name
     Owner = local.owner
   }
+  lifecycle {
+    ignore_changes = [
+      ingress,
+      egress,
+    ]
+  }
 }
 
 # this rule allows ingress on any port from the ip specified
-resource "aws_security_group_rule" "from_ip" {
+resource "aws_vpc_security_group_ingress_rule" "from_ip" {
   count             = (local.type.specific_ip_ingress ? 1 : 0)
-  type              = "ingress"
-  from_port         = 0
-  to_port           = 0
-  protocol          = "-1"
-  cidr_blocks       = ["${local.ip}/32"]
+  ip_protocol       = "-1"
+  cidr_ipv4         = "${local.ip}/32"
   security_group_id = aws_security_group.new[0].id
 }
 # this rule allows egress on any port to the ip specified
-resource "aws_security_group_rule" "to_ip" {
+resource "aws_vpc_security_group_egress_rule" "to_ip" {
   count             = (local.type.specific_ip_egress ? 1 : 0)
-  type              = "egress"
-  from_port         = 0
-  to_port           = 0
-  protocol          = "-1"
-  cidr_blocks       = ["${local.ip}/32"]
+  ip_protocol       = "-1"
+  cidr_ipv4         = "${local.ip}/32"
   security_group_id = aws_security_group.new[0].id
 }
 
 # this rule allows any ip in the cidr on any port to initiate connections to the server
-resource "aws_security_group_rule" "internal_ingress" {
+resource "aws_vpc_security_group_ingress_rule" "internal_ingress" {
   count             = (local.type.internal_ingress ? 1 : 0)
-  type              = "ingress"
-  from_port         = 0
-  to_port           = 0
-  protocol          = "-1"
-  cidr_blocks       = [local.cidr]
+  ip_protocol       = "-1"
+  cidr_ipv4         = local.cidr
   security_group_id = aws_security_group.new[0].id
 }
 # this rule allows the server to initiate connections to any ip in the cidr on any port
-resource "aws_security_group_rule" "internal_egress" {
+resource "aws_vpc_security_group_egress_rule" "internal_egress" {
   count             = (local.type.internal_egress ? 1 : 0)
-  type              = "egress"
-  from_port         = 0
-  to_port           = 0
-  protocol          = "-1"
-  cidr_blocks       = [local.cidr]
+  ip_protocol       = "-1"
+  cidr_ipv4         = local.cidr
   security_group_id = aws_security_group.new[0].id
 }
 # this rule allows any ip in the cidr on any port to initiate connections to the server
-resource "aws_security_group_rule" "project_ingress" {
+resource "aws_vpc_security_group_ingress_rule" "project_ingress" {
   count             = (local.type.project_ingress ? 1 : 0)
-  type              = "ingress"
-  from_port         = 0
-  to_port           = 0
-  protocol          = "-1"
-  cidr_blocks       = [local.vpc_cidr]
+  ip_protocol       = "-1"
+  cidr_ipv4         = local.vpc_cidr
   security_group_id = aws_security_group.new[0].id
 }
 # this rule allows the server to initiate connections to any ip in the cidr on any port
-resource "aws_security_group_rule" "project_egress" {
+resource "aws_vpc_security_group_egress_rule" "project_egress" {
   count             = (local.type.project_egress ? 1 : 0)
-  type              = "egress"
-  from_port         = 0
-  to_port           = 0
-  protocol          = "-1"
-  cidr_blocks       = [local.vpc_cidr]
+  ip_protocol       = "-1"
+  cidr_ipv4         = local.vpc_cidr
   security_group_id = aws_security_group.new[0].id
 }
 # this is necessary if you want to update or install anything from the internet
 # allows server to initiate connections to anywhere
-resource "aws_security_group_rule" "external_egress" {
+resource "aws_vpc_security_group_egress_rule" "external_egress" {
   count             = (local.type.public_egress ? 1 : 0)
-  type              = "egress"
-  from_port         = 0
-  to_port           = 0
-  protocol          = "-1"
-  cidr_blocks       = ["0.0.0.0/0"]
+  ip_protocol       = "-1"
+  cidr_ipv4         = "0.0.0.0/0"
   security_group_id = aws_security_group.new[0].id
 }
-
 # allows anywhere to initiate connections to server
 # WARNING! this exposes your server to the public internet
-resource "aws_security_group_rule" "external_ingress" {
+resource "aws_vpc_security_group_ingress_rule" "external_ingress" {
   count             = (local.type.public_ingress ? 1 : 0)
-  type              = "ingress"
-  from_port         = 0
-  to_port           = 0
-  protocol          = "-1"
-  cidr_blocks       = ["0.0.0.0/0"]
+  ip_protocol       = "-1"
+  cidr_ipv4         = "0.0.0.0/0"
   security_group_id = aws_security_group.new[0].id
 }
