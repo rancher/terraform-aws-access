4d1867e53e77b9dab36a789034e08f8c759802cb
Matt Trachier <matttrach@gmail.com>
2023-12-12 12:48:14 -0600

    fix: Fix ip discover (#19)
    * fix: network acl id will be modified by AWS
    
    Signed-off-by: matttrach <matttrach@gmail.com>
    
    * fix: change how IP is discovered, upgrade workflows, upgrade nix
    
    Signed-off-by: matttrach <matttrach@gmail.com>
    
    * fix: tf fmt
    
    Signed-off-by: matttrach <matttrach@gmail.com>
    
    ---------
    
    Signed-off-by: matttrach <matttrach@gmail.com>

--- examples/override/outputs.tf
+++ examples/override/outputs.tf
@@ -3,17 +3,17 @@ output "vpc" {
 }
 
 output "subnet" {
-    value = module.TestOverride.subnet
+  value = module.TestOverride.subnet
 }
 
 output "cidr" {
-    value = module.TestOverride.cidr
+  value = module.TestOverride.cidr
 }
 
 output "security_group" {
-    value = module.TestOverride.security_group
+  value = module.TestOverride.security_group
 }
 
 output "ssh_key" {
-    value = module.TestOverride.ssh_key
+  value = module.TestOverride.ssh_key
 }
