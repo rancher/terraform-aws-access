5e08fe2ceae52943409de3a5a90f07ca7d14d086
matttrach <matt.trachier@suse.com>
2024-06-27 23:09:40 -0500

    fix: remove extra functions
    Signed-off-by: matttrach <matt.trachier@suse.com>

--- tests/util_test.go
+++ tests/util_test.go
@@ -5,10 +5,6 @@ import (
 	"os"
 	"testing"
 
-	a "github.com/aws/aws-sdk-go/aws"
-	"github.com/aws/aws-sdk-go/service/ec2"
-	aws "github.com/gruntwork-io/terratest/modules/aws"
-	"github.com/gruntwork-io/terratest/modules/random"
 	"github.com/gruntwork-io/terratest/modules/terraform"
 	"github.com/stretchr/testify/require"
 )
@@ -49,71 +45,4 @@ func setup(t *testing.T, directory string, region string, terraformVars map[stri
 		RetryableTerraformErrors: retryableTerraformErrors,
 	})
 	return terraformOptions
-}
-
-func setupKeyPair(t *testing.T, directory string, region string, owner string, uniqueID string) *aws.Ec2Keypair {
-	// Create an EC2 KeyPair that we can find in the module
-	keyPairName := fmt.Sprintf("terraform-aws-access-test-%s-%s", directory, uniqueID)
-	keyPair := aws.CreateAndImportEC2KeyPair(t, region, keyPairName)
-	// tag the key pair
-	client, err1 := aws.NewEc2ClientE(t, region)
-	require.NoError(t, err1)
-	input := &ec2.DescribeKeyPairsInput{
-		KeyNames: []*string{a.String(keyPairName)},
-	}
-	result, err2 := client.DescribeKeyPairs(input)
-	require.NoError(t, err2)
-	aws.AddTagsToResource(t, region, *result.KeyPairs[0].KeyPairId, map[string]string{"Name": keyPairName, "Owner": owner})
-	return keyPair
-}
-
-func teardownKeyPair(t *testing.T, keyPair *aws.Ec2Keypair) {
-	aws.DeleteEC2KeyPair(t, keyPair)
-}
-
-func setupSecurityGroup(t *testing.T, directory string, region string, owner string) (string, string) {
-	// Create an EC2 Security Group that we can find in the module
-	uniqueID := random.UniqueId()
-	name := fmt.Sprintf("terraform-aws-access-test-%s-%s", directory, uniqueID)
-	// assume we are attaching to a default VPC
-	vpc := aws.GetDefaultVpc(t, region)
-
-	client, err1 := aws.NewEc2ClientE(t, region)
-	require.NoError(t, err1)
-	securityGroupInput := &ec2.CreateSecurityGroupInput{
-		Description: a.String(name),
-		GroupName:   a.String(name),
-		VpcId:       a.String(vpc.Id),
-	}
-	securityGroup, err2 := client.CreateSecurityGroup(securityGroupInput)
-	require.NoError(t, err2)
-	aws.AddTagsToResource(t, region, *securityGroup.GroupId, map[string]string{"Name": name, "Owner": owner})
-	securityGroupRuleInput := &ec2.AuthorizeSecurityGroupEgressInput{
-		GroupId: securityGroup.GroupId,
-		IpPermissions: []*ec2.IpPermission{
-			{
-				FromPort:   a.Int64(80),
-				IpProtocol: a.String("tcp"),
-				IpRanges: []*ec2.IpRange{
-					{
-						CidrIp: a.String("10.0.0.0/16"),
-					},
-				},
-				ToPort: a.Int64(80),
-			},
-		},
-	}
-	_, err3 := client.AuthorizeSecurityGroupEgress(securityGroupRuleInput)
-	require.NoError(t, err3)
-	return *securityGroup.GroupId, name
-}
-
-func teardownSecurityGroup(t *testing.T, region string, securityGroupId string) {
-	client, err1 := aws.NewEc2ClientE(t, region)
-	require.NoError(t, err1)
-	input := &ec2.DeleteSecurityGroupInput{
-		GroupId: a.String(securityGroupId),
-	}
-	_, err2 := client.DeleteSecurityGroup(input)
-	require.NoError(t, err2)
 }
