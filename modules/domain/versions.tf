ee7bb82b03cb7fbe2b11384716d759d8f0efbc00
Matt Trachier <matt.trachier@suse.com>
2026-06-17 13:29:02 -0500

    fix: remove unused local provider (#161)
    Signed-off-by: matttrach <matt.trachier@suse.com>

--- modules/domain/versions.tf
+++ modules/domain/versions.tf
@@ -1,10 +1,6 @@
 terraform {
   required_version = ">= 1.5.0"
   required_providers {
-    local = {
-      source  = "hashicorp/local"
-      version = ">= 2.5"
-    }
     aws = {
       source  = "hashicorp/aws"
       version = ">= 5.11"
